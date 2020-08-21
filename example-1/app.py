6fcad622462e4ceb822a749b8ce6541bd5b00e83
marcosricardoss <marcosricardoss@gmail.com>
2020-08-21 12:50:19 -0300

    feat: add examples

---
+++ example-1/app.py
@@ -0,0 +1,10 @@
+import cv2
+import matplotlib.pyplot as plt
+
+print (cv2.__version__)
+
+image = cv2.imread("image.jpg")
+img_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+plt.imshow(img_rgb)
+plt.show()
